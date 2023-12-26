6056a9a311b740d1a9fe498c7a803ccd4885fa4d
DariousVex <darious.vex@gmail.com>
2023-12-26 23:16:32 +0100

    History window added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let history = [];
+
 function display(value) {
   document.getElementById("result").value += value;
 }
@@ -6,8 +8,16 @@ function clearscreen() {
   document.getElementById("result").value = "";
 }
 
+function updateHistory() {
+  let histDisplay = document.getElementById("history");
+  histDisplay.value = history.join("\n");
+}
+
 function calculate(value) {
   let i = document.getElementById("result").value;
   let r = eval(i);
   document.getElementById("result").value = r;
+  let h = i + " = " + r;
+  history.push(h);
+  updateHistory();
 }
